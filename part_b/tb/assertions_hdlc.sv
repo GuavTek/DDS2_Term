b61d02d4c9e63cdc698155cdb63f0658edfb1ef8
GuavTek <36814224+GuavTek@users.noreply.github.com>
2021-04-13 20:15:32 +0200

    Update assertions_hdlc.sv

--- part_b/tb/assertions_hdlc.sv
+++ part_b/tb/assertions_hdlc.sv
@@ -133,7 +133,7 @@ module assertions_hdlc (
   // #13
   // Rx_Overflow, maybe count Rx_NewBytes
   property RX_ovf;
-    StartStop_pattern(Rx) ##2 Rx_ValidFrame ##1 (Rx_NewByte[->130] within ##[*] StartStop_pattern(Rx)) |=> Rx_Overflow;
+    Rx_FlagDetect ##0 !Rx_ValidFrame ##1 (Rx_NewByte[->130] within ##[*] Rx_FlagDetect) |=> Rx_Overflow;
   endproperty
 
   RX_ovf_Assert: assert property (@(posedge Clk) RX_ovf)
